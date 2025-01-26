77c513268a28777a6b6d6956492238604c640f7a
neiderzamora <neiderzamoramoreno@gmail.com>
2025-01-26 12:15:22 -0500

    add doctor service request list

--- apps/service_request/urls.py
+++ apps/service_request/urls.py
@@ -6,7 +6,7 @@ from .views import (
     #AllServiceRequestDetailView,
     
     DoctorServiceResponseCreateView,
-    #DoctorServiceResponseListView,
+    DoctorServiceResponseListView,
     DoctorServiceResponseDetailView,
     DoctorMarkArrivalView,
     ServiceEndCreateView,
@@ -29,7 +29,7 @@ urlpatterns = [
     
     # doctor
     path('doctor/service_request/<uuid:pk>/respond/', DoctorServiceResponseCreateView.as_view(), name='respond_service_request'),
-    #path('doctor/service_request/list/', DoctorServiceResponseListView.as_view(), name='list_doctor_service_responses'),
+    path('doctor/service_request/list/', DoctorServiceResponseListView.as_view(), name='list_doctor_service_responses'),
     path('doctor/service_request/<uuid:pk>/', DoctorServiceResponseDetailView.as_view(), name='detail_doctor_service_response'),
     path('doctor/service_request/<uuid:pk>/arrive/', DoctorMarkArrivalView.as_view(), name='mark_arrival'),
     
